670d46fd4d17de9350adb0f6a3f163e278e5439d
Felix Wischhusen <88275723+Feloped@users.noreply.github.com>
2024-05-13 12:39:49 +0200

    added solution for relaxation

--- sheets/04_mip/exercises/01_tsp/solution_relaxation.py
+++ sheets/04_mip/exercises/01_tsp/solution_relaxation.py
@@ -7,7 +7,68 @@ import typing
 import gurobipy as gp
 import networkx as nx
 
+class _EdgeVariables:
+    """
+    A helper class that manages the variables for the edges.
+    Such a helper class turns out to be useful in many cases.
+    """
 
+    def __init__(self, G: nx.Graph, model: gp.Model):
+        self._graph = G
+        self._model = model
+        self._vars = {
+            (u, v): model.addVar(vtype=gp.GRB.CONTINUOUS, lb=0, ub=1, name=f"edge_{u}_{v}")
+            for u, v in G.edges
+        }
+
+    def x(self, v, w) -> gp.Var:
+        """
+        Return variable for edge (v, w).
+        """
+        if (v, w) in self._vars:
+            return self._vars[v, w]
+        # If (v,w) was not found, try (w,v)
+        return self._vars[w, v]
+
+    def outgoing_edges(self, vertices):
+        """
+        Return all edges&variables that are outgoing from the given vertices.
+        """
+        # Not super efficient, but efficient enough for our purposes.
+        for (v, w), x in self._vars.items():
+            if v in vertices and w not in vertices:
+                yield (v, w), x
+            elif w in vertices and v not in vertices:
+                yield (w, v), x
+
+    def incident_edges(self, v):
+        """
+        Return all edges&variables that are incident to the given vertex.
+        """
+        for n in self._graph.neighbors(v):
+            yield (v, n), self.x(v, n)
+
+    def __iter__(self):
+        """
+        Iterate over all edges&variables.
+        """
+        return iter(self._vars.items())
+
+    def as_graph(self, in_callback: bool = False):
+        """
+        Return the current solution as a graph.
+        """
+        #TODO: modify this function to set the "x" attribute of each edge
+        if in_callback:
+            # If we are in a callback, we need to use the solution from the callback.
+            used_edges = [(vw, self._model.cbGetSolution(x)) for vw, x in self if self._model.cbGetSolution(x) >= 0.01]
+        else:
+            # Otherwise, we can use the solution from the model.
+            used_edges = [(vw, x.X) for vw, x in self if x.X >= 0.01]
+        g = nx.Graph()
+        for e,x in used_edges:
+            g.add_edge(e[0], e[1], x=x)
+        return g
 
 
 class GurobiTspRelaxationSolver:
@@ -29,13 +90,26 @@ class GurobiTspRelaxationSolver:
         ), "Invalid graph"
         self._model = gp.Model()
         # TODO: Implement me!
+        # add the edge variables
+        self._edge_vars = _EdgeVariables(self.graph, self._model)
+        self._enforce_vertex_constraints()
+        self._minimize()
 
+    def _enforce_vertex_constraints(self):
+        #add constraints that every node has degree of 2 
+        for node in self.graph:
+            self._model.addConstr(sum(x for _, x in self._edge_vars.incident_edges(node)) == 2)
+
+    def _minimize(self):
+        # add the minimize objective to model
+        self._model.setObjective(sum(self.graph[e[0]][e[1]]["weight"] * x for e,x in self._edge_vars), gp.GRB.MINIMIZE)
 
     def get_lower_bound(self) -> float:
         """
         Return the current lower bound.
         """
         # TODO: Implement me!
+        return self._model.ObjBound
 
     def get_solution(self) -> typing.Optional[nx.Graph]:
         """
@@ -52,12 +126,14 @@ class GurobiTspRelaxationSolver:
         ```
         """
         # TODO: Implement me!
+        return self._edge_vars.as_graph()
 
     def get_objective(self) -> typing.Optional[float]:
         """
         Return the objective value of the last solution.
         """
         # TODO: Implement me!
+        return self._model.getObjective().getValue()
 
     def solve(self) -> None:
         """
@@ -67,3 +143,20 @@ class GurobiTspRelaxationSolver:
         self._model.Params.LogToConsole = 1
 
         # TODO: Implement me!
+        while True:
+            self._model.optimize()
+            solution = self._edge_vars.as_graph(in_callback=False)
+            comps = list(nx.connected_components(solution))
+            if len(comps) == 1:
+                break # tour is already whole graph
+                
+            # for each component add constraint, that minimum 2 edges leave this component
+            for comp in comps:
+                # only deal with components that are minimum 2 nodes
+                if len(comp) < 2:
+                    continue
+                # now add constraint
+                self._model.addConstr(sum(x for _, x in self._edge_vars.outgoing_edges(comp)) >= 2)
+        
+        if self._model.Status == gp.GRB.OPTIMAL:
+            return None
